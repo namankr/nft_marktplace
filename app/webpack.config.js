998ff9fe4e189537cbaf7dc2f276bd0368c919f4
Paras Gera <paras.gera@uipath.com>
2022-05-11 10:45:29 +0530

    eject webpack from create-react-app

--- app/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const port = process.env.PORT || 3000;
-
-module.exports = {
-  // Webpack configuration goes here
-    resolve: {fallback: { "crypto" : false }}
-};
